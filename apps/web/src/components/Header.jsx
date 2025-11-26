98d47ee4821696ef7d8539a7d121ab5258c72710
qipuh <quipuh@gmail.com>
2025-11-25 22:49:03 -0500

    Fix logo path for production

--- apps/web/src/components/Header.jsx
+++ apps/web/src/components/Header.jsx
@@ -25,10 +25,9 @@ function Header() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          {/* Logo */}
           <Link to="/" className="flex items-center">
             <span className="text-2xl font-bold text-primary">
-              <img className="logo-td" src="../../public/img/tudestino-logo.png" alt="TuDestino Logo" />
+              <img className="logo-td" src="/img/tudestino-logo.png" alt="TuDestino Logo" />
             </span>
           </Link>
 
